aaa7ab96626f98a526f065743c52272f47b196e6
muhammaday123 <anas.yousuf@thinkdigitally.com>
2026-05-11 23:28:44 +0500

    deploy 5-11-2025

--- src/app/components/top-nav/top-nav.ts
+++ src/app/components/top-nav/top-nav.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-top-nav',
-  imports: [RouterLink],
+  imports: [RouterLink, NgIf],
   templateUrl: './top-nav.html',
   styleUrl: './top-nav.css',
 })
 export class TopNav {
+  isMenuOpen = false;
+
   constructor(public router: Router) {}
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu() {
+    this.isMenuOpen = false;
+  }
 }
